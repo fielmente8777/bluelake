8113dd2e612f285bff702654f129a815d5c59dd5
Nishant Verma <137074771+nishantverma0@users.noreply.github.com>
2026-08-31 14:46:38 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
-        <FeatureStrip />
         <AboutUs />
+        <FeatureStrip />
         <RoomsSuites />
         <ExperiencesStrip />
         <Gallery />
